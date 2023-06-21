ea8de4fe536138765e4f0d213c69268f61ba06c2
GianluGiarde <gianlugiarde@gmail.com>
2023-06-21 18:21:21 +0200

    Add: bootstrap & include once classes php

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
-include __DIR__ . "/models/PetProduct.php";
-include __DIR__ . "/models/Food.php";
-include __DIR__ . "/models/Bed.php";
-include __DIR__ . "/models/Hygiene.php";
-include __DIR__ . "/models/Leash.php";
-include __DIR__ . "/models/PetPorter.php";
-include __DIR__ . "/db.php";
+include_once __DIR__ . "/models/Food.php";
+include_once __DIR__ . "/models/Bed.php";
+include_once __DIR__ . "/models/Hygiene.php";
+include_once __DIR__ . "/models/Leash.php";
+include_once __DIR__ . "/models/PetPorter.php";
+include_once __DIR__ . "/db.php";
 ?>
 
 <!DOCTYPE html>
@@ -15,11 +14,25 @@ include __DIR__ . "/db.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"
+        defer></script>
     <title>Prodotti per cani</title>
 </head>
 
 <body>
-    <h1>Prodotti per cani</h1>
+    <div class="container">
+        <h2>Prodotti per animali</h2>
+        <hr>
+        <?php
+        foreach ($products as $product) { ?>
+        <ul>
+        </ul>
+        <hr>
+        <?php } ?>
+    </div>
 </body>
 
 </html>
