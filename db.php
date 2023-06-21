9c00bddb5200216dd863f9c9759419c78b00f637
GianluGiarde <gianlugiarde@gmail.com>
2023-06-21 18:24:58 +0200

    Fix: food

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 $products = [
     new Food(00001, "Crocchette", "img-crocchette", 1999, "dog", "dry", "01/2024"),
     new Food(00010, "Mix: carne-pollo-manzo", "img-carne-pollo-manzo", 3199, "dog", "weet", "11/2023"),
-    new Food(00011, "Mix: salmone-pollo", 2499, "img-carne-pollo-manzo", "cat", "weet", "10/2023"),
+    new Food(00011, "Mix: salmone-pollo", "img-carne-pollo-manzo", 2499, "cat", "weet", "10/2023"),
     new Food(00100, "Crocchette", "img-crocchette-gatti", 1799, "cat", "dry", "02/2024"),
     new Bed(00101, "Cuccia-per-cani-grande", "img-cuccia-cane", 3499, "dog", "large", "light-grey", "cottone"),
     new Bed(00111, "Cuccia-per-gatti-piccola", "img-cuccia-gatto", 2599, "cat", "small", "red", "polyester"),
