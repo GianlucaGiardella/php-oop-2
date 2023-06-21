56f11a7668c58e95045e8ccf31593e678d06cb04
GianluGiarde <gianlugiarde@gmail.com>
2023-06-22 00:14:15 +0200

    Change: language / Add: size / Fix: some problem

--- db.php
+++ db.php
@@ -1,16 +1,16 @@
 <?php
 
 $products = [
-    new Food(00001, "Crocchette", "img-crocchette", 1999, "dog", "dry", "01/2024"),
-    new Food(00010, "Mix: carne-pollo-manzo", "img-carne-pollo-manzo", 3199, "dog", "weet", "11/2023"),
-    new Food(00011, "Mix: salmone-pollo", "img-carne-pollo-manzo", 2499, "cat", "weet", "10/2023"),
-    new Food(00100, "Crocchette", "img-crocchette-gatti", 1799, "cat", "dry", "02/2024"),
-    new Bed(00101, "Cuccia-per-cani-grande", "img-cuccia-cane", 3499, "dog", "large", "light-grey", "cottone"),
-    new Bed(00111, "Cuccia-per-gatti-piccola", "img-cuccia-gatto", 2599, "cat", "small", "red", "polyester"),
-    new Health(01000, "Spazzola-per-cani", "img-spzzola-cane", 1199, "dog", "medium", "grey", "plastic"),
-    new Health(01001, "Lettiera-per-gatti", "img-lettiera-gatto", 2999, "cat", "large", "black", "plastic"),
-    new Leash(01011, "Guinzaglio-per-cani", "img-guinzaglio-cane", 999, "dog", "large", "black-white", "acrylic", 2000),
-    new Leash(01111, "Guinzaglio-per-gatti", "img-guinzaglio-gatto", 1099, "cat", "medium", "black-red", "nnylon", 3000),
-    new PetPorter(10000, "Trasportino-per-cani", "img-trasportino-cane", 4499, "dog", "large", "black", "metal-polyester"),
-    new PetPorter(10001, "Trasportino-per-gatti", "img-trasportino-gatto", 2499, "cat", "small", "beige", "plastic")
+    new Food(00001, "Crocchette", "img-crocchette", 1999, "cane", "piccola", "asciutto", "01/2024"),
+    new Food(00010, "Mix: carne-pollo-manzo", "img-carne-pollo-manzo", 3199, "cane", "grande", "umido", "11/2023"),
+    new Food(00011, "Mix: salmone-pollo", "img-carne-pollo-manzo", 2499, "gatto", "media", "umido", "10/2023"),
+    new Food(00100, "Crocchette", "img-crocchette-gatti", 1799, "gatto", "piccola", "asciutto", "02/2024"),
+    new Bed(00101, "Cuccia-per-cani-grande", "img-cuccia-cane", 3499, "cane", "grande", "grigio", "cotone"),
+    new Bed(00111, "Cuccia-per-gatti-piccola", "img-cuccia-gatto", 2599, "gatto", "piccola", "rosso", "poliestere"),
+    new Health(01000, "Spazzola-per-cani", "img-spzzola-cane", 1199, "cane", "media", "azzurro", "plastica"),
+    new Health(01001, "Lettiera-per-gatti", "img-lettiera-gatto", 2999, "gatto", "grande", "nero", "plastica"),
+    new Leash(01011, "Guinzaglio-per-cani", "img-guinzaglio-cane", 999, "cane", "grande", "nero e bianco", "acrilico", 2000),
+    new Leash(01111, "Guinzaglio-per-gatti", "img-guinzaglio-gatto", 1099, "gatto", "media", "nero e rosso", "nylon", 3000),
+    new PetPorter(10000, "Trasportino-per-cani", "img-trasportino-cane", 4499, "cane", "grande", "nero", "metallo-poliestere"),
+    new PetPorter(10001, "Trasportino-per-gatti", "img-trasportino-gatto", 2499, "gatto", "piccola", "beige", "plastica")
 ];
